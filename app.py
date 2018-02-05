687871646eaf43992854880b0f8cb824cd5cf5a6
押野泰平 <toshino@oshinotaiheinoMacBook-Pro.local>
2018-02-05 15:24:55 +0900

    add /echo

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -8,6 +10,13 @@ def index():
 @app.route('/data')
 def insert():
     data = request.args.get('data', type = str)
+
+@app.route('/echo')
+def echo():
+    output = {
+        "speech": "hello"
+    }
+    return json.dumps(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
